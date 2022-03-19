aaf40bc42763fb46e7f6816b0980a8b18b376a7d
Spacekid2260 <98662444+Spacekid2260@users.noreply.github.com>
2022-03-19 12:21:15 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . 3 . . 1 . . . . . 
+. . . . . 2 . . . . . . . . . . 
+. . . e . . . . . . . 4 . . . . 
+. . . . . . . . . . . . . . . . 
+. . c . . . f f f f . . . . . . 
+. . . . . . f 2 2 f . . 5 . . . 
+. . a . . . f 2 2 f . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . 9 . . . . . . . 6 . . . . 
+. . . . . d . . 7 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
